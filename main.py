6d75e815dd30240ac547efa08f6b40fb017eba97
Timur Shatanov <58140315+shatanov@users.noreply.github.com>
2022-03-23 22:30:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 def parser():
     for i in range(1, 1000):
         response = requests.get(
-            "https://bbb209.ssau.ru/bigbluebutton/presentation/af5bce74099e83601eef9aaa04363150faac917e-1647967370646/af5bce74099e83601eef9aaa04363150faac917e-1647967370646/a2dcc06f8c55aadb43521709d5b1421890ada168-1647967454445/svg/" + str(
+            "" + str(
                 i))
         if response.status_code != 404:
             out = open("photo/img" + str(i) + ".svg", "wb")
