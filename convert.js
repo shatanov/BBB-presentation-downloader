f7bf3d58c1e699e605279b4eb23ee9f1bc5327f4
shatanov <timur-shatanov.work@mail.ru>
2022-03-25 18:11:08 +0300

    fix convert.js

--- convert.js
+++ convert.js
@@ -2,21 +2,24 @@ const sharp = require("sharp")
 const fs = require('fs');
 
 const testFolder = './photo/';
+
+const convertToPng = (file) => {
+    const svgFile = file;
+    const pngFile = file.replace(".svg", ".png");
+    sharp("photo/"+svgFile)
+    .png()
+    .toFile("photo_png/"+pngFile)
+    .then(function(info) {
+        console.log(info)
+    })
+    .catch(function(err) {
+        console.log(err)
+    })
+};
+
 fs.readdir(testFolder, (err, files) => {
     for(let file of files) {
-        const svgFile = file;
-        const pngFile = file.replace(".svg", ".png");
-        sharp("photo/"+svgFile)
-        .png()
-        .toFile("photo_png/"+pngFile)
-        .then(function(info) {
-            console.log(info)
-        })
-        .catch(function(err) {
-            console.log(err)
-        })
+        convertToPng(file);
     }
 });
-
-
 
