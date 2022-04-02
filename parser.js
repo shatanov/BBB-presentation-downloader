1379191d2a3954b68061847ef8a3bb50d5c9705f
Timur Shatanov <58140315+shatanov@users.noreply.github.com>
2022-04-02 11:40:31 +0300

    fix

--- parser.js
+++ parser.js
@@ -5,11 +5,10 @@ const options = {
     url: '',
     dest: '../../photo/img.svg',
 }
-let errCode = 0;
 download.image(options)
     .then(({ filename }) => {
         console.log('Saved to', filename)
     })
     .catch((err) => {
-        errCode = (err.message.split("Status Code: ").pop())
+        (err.message.split("Status Code: ").pop())
     });
