dbac0370151735765854c9c2c3421e78812cf9c6
shatanov <timur-shatanov.work@mail.ru>
2022-04-05 23:25:31 +0300

    add parser into http

--- parser.js
+++ parser.js
@@ -1,16 +1,39 @@
 'use strict';
 
-const download = require('image-downloader');
+const fs = require('fs');
+const client = require('https');
+
+const downloadImage = (url, filepath) => {
+    return new Promise((resolve, reject) => {
+        client.get(url, (res) => {
+            if (res.statusCode === 200) {
+                res.pipe(fs.createWriteStream(filepath))
+                    .on('error', reject)
+                    .once('close', () => resolve(filepath));
+            } else {
+                res.resume();
+                console.log("h");
+                reject(new Error(`Request Failed With a Status Code: ${res.statusCode}`));
+            }
+        });
+    });
+};
+
+
+
+function findPhoto(options) {
+    for (let i = 0; i < options.files; i++) {
+        downloadImage(options.url, options.path)
+    }
+};
+
 
 
 const options = {
-    url: '',
-    dest: '../../photo/img.svg',
-}
-download.image(options)
-    .then(({ filename }) => {
-        console.log('Saved to', filename)
-    })
-    .catch((err) => {
-        (err.message.split("Status Code: ").pop())
-    });
+    url: 'https://sun9-69.userapi.com/impf/UxOOwlwyxP1HyYKa1ysbLgWD5GVJBGXn_ErDuw/xU8LFOUu_Lg.jpg?size=1920x1080&quality=96&sign=8146b4d8831e1107dae73fb14542897c&type=album',
+    path: 'photo/img.png',
+    files: 10
+};
+
+findPhoto(options)
+
